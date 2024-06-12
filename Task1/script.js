1e0576330ebd440d481976a3ad657866bcc149fe
Sivakkanth <sivakanth2000s@gmail.com>
2024-06-13 02:58:07 +0530

    Finished Task 1

--- Task1/script.js
+++ Task1/script.js
@@ -17,3 +17,12 @@ backBtn.addEventListener("click", ()=>{
     scrollContainer.style.scrollBehavior = "smooth";
     scrollContainer.scrollLeft -= 900;
 })
+
+const toTop = document.querySelector('.to-top');
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    }else{
+        toTop.classList.remove("active")
+    }
+})
